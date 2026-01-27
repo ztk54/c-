2f1c283bce5ab5f3301154264d685366e88199ca
proud-even-more-than-oreo <19137037322@163.com>
2026-01-27 18:08:32 +0800

    修改了自己的解决版本，可以正常进行运行

--- c语言学习/c语言学习/餐厅打分.c
+++ c语言学习/c语言学习/餐厅打分.c
@@ -1,55 +1,63 @@
+//我的解答版本
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #define N 40
-void Output(int evaluate[], int n);
+void Output(int evaluate[], int m);
 int Intput(int evaluate[]);
 int main()
 {
-    int evaluate[N];
-    int m = Intput(evaluate);
-    Output(evaluate, m);
-    return 0;
+	int evaluate[N];
+	int m = Intput(evaluate);
+	Output(evaluate, m);
+	return 0;
 }
 
 void Output(int evaluate[], int m)
 {
-    printf("Grade   Count   Histogram\n");
-
-    int count[11] = { 0 }; 
-    for (int i = 0; i < m; i++)
-    {
-        if (evaluate[i] >= 1 && evaluate[i] <= 10)
-        {
-            count[evaluate[i]]++;
-        }
-    }
-
-    for (int grade = 1; grade <= 10; grade++)
-    {
-        printf("%d\t%d\t", grade, count[grade]);
-        for (int j = 0; j < count[grade]; j++)
-        {
-            printf("* ");
-        }
-        printf("\n");
-    }
+	printf("Grade   Count   Histogram\n");
+	for (int i = 0;i < m;i++)
+	{
+		printf("%d\t%d\t", i + 1, evaluate[i]);
+		for (int j = 0;j < evaluate[i];j++)
+		{
+			if (evaluate[i] - j == 1)
+			{
+				printf("*\n");
+			}
+			else
+			{
+				printf("*");
+			}
+		}
+	}
 }
+
 int Intput(int evaluate[])
 {
-    printf("请输入40个学生的评分（1-10分）：\n");
-
-    int i = 0;
-    while (i < N)
-    {
-        printf("请输入第%d个学生的评分: ", i + 1);
-        scanf("%d", &evaluate[i]);
-        if (evaluate[i] < 1 || evaluate[i] > 10)
-        {
-            printf("评分有误，请输入1-10之间的数字！\n");
-            continue;
-        }
-        i++; 
-    }
-    printf("已完成40个学生的评分输入。\n");
-    return N; 
+	int i = 0;
+	int order = 1;
+	do
+	{
+		printf("请您决定是否继续评分，输入1继续评分，输入0退出评分系统\n");
+		scanf("%d", &order);
+		if (order == 0)
+		{
+			break;
+		}
+		printf("请输入第%d人的评分:\n", i + 1);
+		scanf("%d", &evaluate[i]);
+		printf("第%d个人的评分为%d\n", i + 1, evaluate[i]);
+		if ((evaluate[i] > 10) || (evaluate[i] < 0))
+		{
+			printf("评分错误，请重新进行输入评分\n");
+			continue;
+		}
+		i++;
+		if (i == 40)
+		{
+			printf("所有的评分已录入\n");
+			break;
+		}
+	} while (i < 40);
+	return i;
 }
