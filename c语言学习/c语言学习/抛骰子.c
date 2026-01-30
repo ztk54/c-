d50bf96e0fdceb6cdd083344233b5837cd2f1c8f
proud-even-more-than-oreo <19137037322@163.com>
2026-01-30 22:54:23 +0800

    1.30

--- c语言学习/c语言学习/抛骰子.c
+++ c语言学习/c语言学习/抛骰子.c
@@ -1,21 +1,21 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <time.h>
-//int main()
-//{
-//	int frequency[7] = { 0 };
-//	srand(time(NULL));
-//	int roll,face;
-//	for (roll = 1;roll <=6000;roll++)
-//	{
-//		face = rand() % 6 + 1;
-//		frequency[face]++;
-//	}
-//	printf("%4s%17s\n", "Face", "Frequency");
-//	for (int face = 1;face < 7;face++)
-//	{
-//		printf("%4d%17d\n", face, frequency[face]);
-//	}
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+	int frequency[7] = { 0 };
+	srand(time(NULL));
+	int roll,face;
+	for (roll = 1;roll <=6000;roll++)
+	{
+		face = rand() % 6 + 1;
+		frequency[face]++;
+	}
+	printf("%4s%17s\n", "Face", "Frequency");
+	for (int face = 1;face < 7;face++)
+	{
+		printf("%4d%17d\n", face, frequency[face]);
+	}
+	return 0;
+}
