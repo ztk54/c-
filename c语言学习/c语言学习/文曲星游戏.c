f31b3dfd2e6d4ad727782c2e08c224b88350f534
proud-even-more-than-oreo <19137037322@163.com>
2026-02-05 17:49:47 +0800

    文曲星新增了检查数字和生成数字功能

--- c语言学习/c语言学习/文曲星游戏.c
+++ c语言学习/c语言学习/文曲星游戏.c
@@ -3,18 +3,63 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
-int Creat_the_number();
-void Give_the_tip();
+int Creat_the_number(int real_digits[]);
+int Check_the_number(int n,int guess_digits[]);
 int main()
 {
-	
+	int real_digits[4],guess_digits[4];
+	int the_real_number=Creat_the_number(real_digits);
+	printf("你想猜几次");
+	int wanted_count = 0;
+	int real_count = 0;
+	scanf("%d", &wanted_count);
+	do
+	{
+		int the_guess_number=0;
+		scanf("%d", &the_guess_number);
+		printf("第%d次机会，请输入一个互不相同的4位数\n",real_count+1);
+		int m=Check_the_number(the_guess_number,guess_digits);
+		real_count++;
+		if (m == 1)
+		{
+			real_count--;
+			printf("输入错误，请您重新输入");
+			continue;
+		}
+	} while (real_count < wanted_count);
+}
+
+int Check_the_number(int n,int digit[])
+{
+	if (n < 1000 || n>9999)
+	{
+		return 1;
+	}
+	else
+	{
+		for (int i = 0;i < 4;i++)
+		{
+			digit[i] = n % 10;
+			n /= 10;
+		}
+		for (int i = 0;i < 4;i++)
+		{
+			for (int j =0 ;j <i;j++)
+			{
+				if (digit[i] ==digit[j])
+				{
+					return 1;
+				}
+			}
+		}
+	}
+	return 0;
 }
 
 //生成一个四位数均不相同的四位数，使用bool类型来生成随机数字
-int Creat_the_number()
+int Creat_the_number(int digits[])
 {
 	srand(time(NULL));
-	int digits[4];
 	bool used[10] = {true};
 	int firstDigit = 1 + rand() % 9;
 	digits[0] = firstDigit;
@@ -30,11 +75,6 @@ int Creat_the_number()
 		used[digit] = true;
 	}
 	int result = digits[0] * 1000 + digits[1] * 100 + digits[2] * 10 + digits[3];
-	printf("生成的四位数为%d", result);
 	return result;
 }
 
-void Give_the_tip()
-{
-
-}
