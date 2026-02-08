4d3309094515a3900caac509ac6146e6a582be93
proud-even-more-than-oreo <19137037322@163.com>
2026-02-08 14:48:03 +0800

    if语句作业

---
+++ c语言学习/c语言学习/if语句作业.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	for (int i = 1;i <= 100;i++)
+//	{
+//		if (i % 3 == 0)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[3];
+//	printf("请输入三个整数");
+//	for (int i = 0;i < 3;i++)
+//	{
+//		scanf("%d", &a[i]);
+//	}
+//	for (int i = 0;i < 3;i++)
+//	{
+//		for (int j = i;j < 3;j++)
+//		{
+//			if (a[i] < a[j])
+//			{
+//				int tem = a[i];
+//				a[i] = a[j];
+//				a[j] = tem;
+//			}
+//		}
+//	}
+//	for (int i = 0;i < 3;i++)
+//	{
+//		printf("%d ", a[i]);
+//	}
+//}
