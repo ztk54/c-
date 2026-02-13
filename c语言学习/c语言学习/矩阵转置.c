3f4210eef6e734568b114c08d8937448d7e0302c
proud-even-more-than-oreo <19137037322@163.com>
2026-02-13 17:02:42 +0800

    转置

---
+++ c语言学习/c语言学习/矩阵转置.c
@@ -0,0 +1,27 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h> 
+//转置矩阵
+//int main()
+//{
+//    int m, n;
+//    scanf("%d %d", &m, &n);
+//    int tem[20][20], final[20][20];
+//    for (int i = 0;i < m;i++)
+//    {
+//        for (int j = 0;j < n;j++)
+//        {
+//            scanf("%d", &tem[i][j]);
+//            final[j][i] = tem[i][j];
+//        }
+//    }
+//    for (int i = 0;i < n;i++)
+//    {
+//        for (int j = 0;j < m;j++)
+//        {
+//            printf("%d ", final[i][j]);
+//        }
+//        printf("\n");
+//    }
+//}
+//
+//
