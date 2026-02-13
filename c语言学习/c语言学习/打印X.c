404dd28fab52ced1477eda9026b0ae16e2cdbfaa
proud-even-more-than-oreo <19137037322@163.com>
2026-02-13 11:42:18 +0800

    打印X.c

---
+++ c语言学习/c语言学习/打印X.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+    int a=0;
+    while (scanf("%d",&a) != EOF)
+    {
+        if (a % 2 == 1)
+        {
+            int tem1 = a - 2;
+            int tem2 = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                if (i < (a + 1) / 2)
+                {
+                    for (int j = 1; j < i; j++)
+                    {
+                        printf(" ");
+                    }
+                    printf("*");
+                    for (int j = 1; j <= tem1; j++)
+                    {
+                        printf(" ");
+                    }
+                    printf("*");
+                    for (int j = 1; j < i; j++)
+                    {
+                        printf(" ");
+                    }
+                    printf("\n");
+                    tem1 -= 2;
+                }
+                else if (i == (a + 1) / 2)
+                {
+                    for (int j = 1;j <= a / 2;j++)
+                    {
+                        printf(" ");
+                    }
+                    printf("*");
+                    for (int j = 1;j <= a / 2;j++)
+                    {
+                        printf(" ");
+                    }
+                    printf("\n");
+                }
+                else if (i > (a + 1) / 2)
+                {
+                    for (int j = 1;j <= a - i;j++)
+                    {
+                        printf(" ");
+                    }
+                    printf("*");
+                    for (int j = 1;j <= tem2;j++)
+                    {
+                        printf(" ");
+                    }
+                    printf("*");
+                    for (int j = 1;j <= a - i;j++)
+                    {
+                        printf(" ");
+                    }
+                    printf("\n");
+                    tem2 += 2;
+                }
+            }
+        }
+    }
+}
