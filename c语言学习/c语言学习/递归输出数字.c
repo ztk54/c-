dd4d1d56aaa5c07eb6b7b141b900a9f81d9411f3
proud-even-more-than-oreo <19137037322@163.com>
2026-03-04 19:05:56 +0800

    逐个输出数字

---
+++ c语言学习/c语言学习/递归输出数字.c
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//void Print(int n)
+//{
+//	if (n > 9)
+//	{
+//		Print(n / 10);
+//	}
+//	printf("%d ", n % 10);
+//}
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	Print(n);
+//}
