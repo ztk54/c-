742e30ae821728f4aa56d1104cb83937a81828bc
proud-even-more-than-oreo <19137037322@163.com>
2026-03-08 22:47:16 +0800

    &，|的应用

--- c语言学习/c语言学习/3.7.c
+++ c语言学习/c语言学习/3.7.c
@@ -1,10 +1,27 @@
-#define _crt_secure_no_warnings
-#include <stdio.h>
-int main()
-{
-	int num = 10;
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+	/*int num = 10;
 	int r = num << 1;
 	printf("r=%d\n", r);
 	printf("num=%d", num);
-	return 0;
-}
+	return 0;*/
+	//int num = 0;
+	//scanf("%d", &num);
+	//int count = 0;
+	//for (int i = 0;i < 32;i++)
+	//{
+	//	if (((num >> i)&1) == 1)
+	//	{
+	//		count++;
+	//	}
+	//}
+//	while (num)
+//	{
+//		num = num & (num - 1);
+//		count++;
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
